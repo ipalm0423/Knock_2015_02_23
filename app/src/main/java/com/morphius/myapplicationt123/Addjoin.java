2577112432a5dfa96c01d6701d2077ffc8bea0a3
ipalm0423 <76423ok@gmail.com>
2015-02-23 17:14:03 +0800

    update addjoin UI

--- app/src/main/java/com/morphius/myapplicationt123/Addjoin.java
+++ app/src/main/java/com/morphius/myapplicationt123/Addjoin.java
@@ -5,10 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * Created by 2 on 2015/2/23.
- */
+
 public class Addjoin extends Activity {
 
     @Override
@@ -33,6 +34,44 @@ public class Addjoin extends Activity {
                 intent.setClass(Addjoin.this, MainActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+        final SeekBar seekBartimer = (SeekBar)findViewById(R.id.seekBartimer);
+        final TextView textViewtimer = (TextView)findViewById(R.id.textViewtimebar);
+
+        seekBartimer.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewtimer.setText(seekBartimer.getProgress() + "Hours");
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (seekBartimer.getProgress() <= 24){
+                    Toast.makeText(Addjoin.this, seekBartimer.getProgress() + " Hours", Toast.LENGTH_SHORT).show();
+                }
+                else if (seekBartimer.getProgress() > 24 ) {
+                    int time = seekBartimer.getProgress();
+                    int days = time / 24;
+                    int hours = time % 24;
+                    Toast.makeText(Addjoin.this, days + " Days and " + hours + " Hours", Toast.LENGTH_SHORT).show();
+
+                }
+                else if (seekBartimer.getProgress() > 720) {
+                    int time = seekBartimer.getProgress();
+                    int days = (time % 720) / 24;
+                    int hours = (time % 720) % 24;
+                    int month = time / 720;
+                    Toast.makeText(Addjoin.this, month + " Months, " + days + " Days, " + hours + " Hours", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
     }
